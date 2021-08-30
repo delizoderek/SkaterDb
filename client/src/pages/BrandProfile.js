dbd947070c17a846471655028bfe9c2de0b9bbf5
Simran Kaur <kaursim.2019@yahoo.com>
2021-08-29 22:25:46 -0700

    pages set up

---
+++ client/src/pages/BrandProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrandProfile = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BrandProfile
---
+++ client/src/pages/SkaterProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const SkaterProfile = () => {
+    return (
+        <div>
+            <Navbar/>
+            
+        </div>
+    )
+}
+
+export default SkaterProfile
